7ea63711e6eef433fdf4f6f726df3648ff1fdc3a
antonio-emilio <antoniopereira@discente.ufg.br>
2023-12-28 23:03:34 -0300

    feat

--- main.py
+++ main.py
@@ -1,56 +1,41 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_restful.reqparse import RequestParser
 import cv2
-from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.lang import Builder
+import numpy as np
+import base64
 from kivy.uix.image import Image
-from kivy.core.window import Window
 from kivy.graphics.texture import Texture
 from kivy.clock import Clock
 from ultralytics import YOLO
-from PIL import Image as PILImage
 
+app = Flask(__name__)
+api = Api(app)
 model = YOLO("gd.pt")
 
-class MainScreen(Screen):
-    pass
-
-Builder.load_file("screen.kv")
-
-class Main(App):
-
-    def build(self):
-        sm = ScreenManager()
-        self.main_screen = MainScreen(name="main")
-        sm.add_widget(self.main_screen)
-        self.image = Image(
-            size_hint=(None, None),
-            size=(Window.width * 1, Window.height * 1),
-            center=(Window.width * 0.5, Window.height * 0.5)
-        )
-        self.main_screen.add_widget(self.image, 1)
-
-        # Carregar imagem da área de trabalho
-        image_path = "pessoa.jpg"  # Substitua pelo caminho da sua imagem
-        self.image_frame = cv2.imread(image_path)
-        texture = self.convert_frame_to_texture(self.image_frame)
-        self.image.texture = texture
-
+class InferenceResource(Resource):
+    def __init__(self):
         self.inference_count = 0
-        Clock.schedule_interval(self.inference, 1 / 10)
-        return sm
+        self.image_frame = None
+        self.image_texture = None
 
-    def inference(self, *args):
+    def post(self):
+        parser = RequestParser()
+        parser.add_argument('image', type=str, required=True)
+        args = parser.parse_args()
+
+        image_data = args['image'].split(',')[1]  # Remove o cabeçalho da string base64
+        image_decoded = cv2.imdecode(np.frombuffer(base64.b64decode(image_data), np.uint8), cv2.IMREAD_COLOR)
+
+        self.image_frame = image_decoded
+
+        # Realizar uma única inferência
         pixelated_frame = self.pixelate_frame(self.image_frame, 3)
         results = model.predict(source=pixelated_frame, verbose=False)
-        People_count = len(results[0].boxes)
-        print(f'Pessoas encontradas na inferência {self.inference_count + 1}: {People_count}')
-        self.inference_count += 1
-        App.get_running_app().stop()  # Finaliza a execução após 5 inferências
+        people_count = len(results[0].boxes)
+        print(f'Pessoas encontradas na inferência: {people_count}')
 
-    def pixelate_frame(self, frame, block_size):
-        small_frame = cv2.resize(frame, None, fx=1 / block_size, fy=1 / block_size, interpolation=cv2.INTER_NEAREST)
-        pixelated_frame = cv2.resize(small_frame, (frame.shape[1], frame.shape[0]), interpolation=cv2.INTER_NEAREST)
-        return pixelated_frame
+        return {'people_count': people_count}
 
     def convert_frame_to_texture(self, frame):
         buffer = cv2.flip(frame, 0).tostring()
@@ -58,5 +43,12 @@ class Main(App):
         texture.blit_buffer(buffer, colorfmt='bgr', bufferfmt='ubyte')
         return texture
 
+    def pixelate_frame(self, frame, block_size):
+        small_frame = cv2.resize(frame, None, fx=1 / block_size, fy=1 / block_size, interpolation=cv2.INTER_NEAREST)
+        pixelated_frame = cv2.resize(small_frame, (frame.shape[1], frame.shape[0]), interpolation=cv2.INTER_NEAREST)
+        return pixelated_frame
+
+api.add_resource(InferenceResource, '/inference')
+
 if __name__ == '__main__':
-    Main().run()
+    app.run(debug=True)
