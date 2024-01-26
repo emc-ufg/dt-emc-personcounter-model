cabfeba2680ac42a3fb085122d9567b8f83959de
antonio-emilio <antonio_pereira@outlook.com>
2024-01-26 12:23:10 +0000

    feat

--- main.py
+++ main.py
@@ -45,6 +45,12 @@ class InferenceResource(Resource):
         people_count = len(results[0].boxes)
         print(f'Pessoas encontradas na inferência: {people_count}')
 
+        # Concat the random number and letter to the image filename
+        image_filename = f"received_image_{random_number}_{random_letter}.png"
+
+        cv2.imwrite(image_filename, image_decoded)
+        print(f'Imagem salva como: {image_filename}')
+
         # Enviar resultado para o tópico MQTT
         self.send_mqtt_message(people_count)
 
